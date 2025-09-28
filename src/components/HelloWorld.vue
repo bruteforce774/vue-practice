4108719b11ccf9efd1127f8fe6bfc67e2eb39d04
bruteforce774 <danielaw@getacademy.no>
2025-09-28 05:25:59 +0200

    v-show

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,12 @@ import { ref } from 'vue'
 
 defineProps(['msg'])
 
-const count = ref(0);
+const count = ref(9)
 </script>
 
 <template>
     <div>
-        <h1>{{ msg }}</h1>
+        <h1 v-show="count<10">Mindre enn 10</h1>
 
         <div>
             <input type="range" min="1" max="100" v-model="count"></input>
